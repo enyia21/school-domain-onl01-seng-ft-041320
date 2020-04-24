ff1c804163e1525f643d2ee7765f567e6ca86909
Chibuisi Enyia <enyia21@gmail.com>
2020-04-24 12:46:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,12 @@
 # code here!
-class School(name)
+class School 
+  
+  def initialize(roster={})
+    @roster = roster
+  end
+
+  def add_student=(name, grade)
+    school_roster_hash = []
+    school_roster_hash[grade] << 
+    
 end
