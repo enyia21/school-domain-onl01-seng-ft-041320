193bfbfd62dfb2ab3cbc47c9d9aee059724ddac0
Chibuisi Enyia <enyia21@gmail.com>
2020-04-24 14:01:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,9 +10,12 @@ class School
   end
 
   def add_student(name, grade)
-
-      @roster[grade] = []
-      @roster[grade] << name
+      if @roster[grade] == nil
+        @roster[grade] = []
+        @roster[grade] << name
+      else 
+        @roster[grade] << name
+        
 
   end
 
